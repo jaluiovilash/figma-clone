a31be8a5254d113fca7fe6d3a508addff64496d9
jaluiovilash <ovilashjalui@gmail.com>
2024-08-23 02:04:04 +0530

    updated live cursor

---
+++ components/Live.tsx
@@ -0,0 +1,53 @@
+import React, { useCallback } from 'react'
+import LiveCursors from './cursor/LiveCursors'
+import { useMyPresence, useOthers } from '@liveblocks/react';
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback(
+    (event: React.PointerEvent) => {
+      event.preventDefault();
+
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+      updateMyPresence({ cursor: { x, y } });
+    },
+    [],
+  )
+
+  const handlePointerLeave = useCallback(
+    (event: React.PointerEvent) => {
+      event.preventDefault();
+
+      updateMyPresence({ cursor: null, message: null });
+    },
+    [],
+  )
+
+  const handlePointerDown = useCallback(
+    (event: React.PointerEvent) => {
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+      updateMyPresence({ cursor: { x, y } });
+    },
+    [],
+  )
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center"
+    >
+      <h1 className="font-5xl text-white">figma clone</ h1>
+      <LiveCursors others={others} />
+    </div>
+  )
+}
+
+export default Live
